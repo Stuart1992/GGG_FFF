e3a4ecb02066ac946cd45d382ff7bd7839d79d8b
pkenney <ricefrog@gmail.com>
2015-01-24 00:03:52 -0500

    added girl

---
+++ Assets/Scripts/JumpInputController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpInputController : MonoBehaviour {
+
+	//docs say this should work but no dice - assume it is a problem with 2D vs 3D box colliders??
+	//void OnMouseDown()
+	//{
+	//	Debug.Log ("YOU CLICKED ON THE GIRL!");
+	//}
+	
+	private bool isTrackingMouse=false;
+
+	void Start () {
+		
+	}	
+	
+	void Update () {
+				
+		if ( Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, hit, 100.0))
+			{
+				Destroy(GameObject.Find("targetArea"));
+			}
+		}
+		
+	}
+}
