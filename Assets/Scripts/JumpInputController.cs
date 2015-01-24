1993d015d2f799eae7d0cc8e295fc3bd2a5fe6fb
pkenney <ricefrog@gmail.com>
2015-01-24 02:11:08 -0500

    Added user input for setting arrow vectors on children

--- Assets/Scripts/JumpInputController.cs
+++ Assets/Scripts/JumpInputController.cs
@@ -3,29 +3,54 @@ using System.Collections;
 
 public class JumpInputController : MonoBehaviour {
 
-	//docs say this should work but no dice - assume it is a problem with 2D vs 3D box colliders??
-	//void OnMouseDown()
-	//{
-	//	Debug.Log ("YOU CLICKED ON THE GIRL!");
-	//}
-	
-	private bool isTrackingMouse=false;
+	public Transform ArrowPrefab;
 
+	private Transform arrow;
+	private Vector2 aimPoint;
+	private bool isTrackingMouse=false;
+	
+	
+	//docs say this should work but no dice - assume it is a problem with 2D vs 3D box colliders??
+	void OnMouseDown()
+	{
+		isTrackingMouse = true;
+		Debug.Log ("YOU CLICKED ON THE GIRL!");
+	}
+	
 	void Start () {
-		
+		arrow = (Transform) GameObject.Instantiate(ArrowPrefab);
+		arrow.position = new Vector3(999,999,0);
 	}	
 	
 	void Update () {
-				
-	/*	if ( Input.GetMouseButtonDown(0))
+		if(isTrackingMouse)
 		{
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, hit, 100.0))
+			if(Input.GetMouseButtonUp(0))
 			{
-				Destroy(GameObject.Find("targetArea"));
+				isTrackingMouse = false;
 			}
-		}
-	*/	
+			else
+			{
+				aimPoint =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				PointArrowToAimPoint();
+			}
+				
+		}			
+
+		
 	}
+	
+	private void PointArrowToAimPoint()
+	{
+		Vector2 s = transform.position;
+		Vector2 e = aimPoint;
+		Vector2 diff = (s-e);
+		arrow.position = (s+e)/2; // midpoint between start and end		
+		arrow.localScale =  new Vector3(arrow.localScale.x, -diff.magnitude/10, arrow.localScale.z);
+		
+		float aimangle = Mathf.RoundToInt(Mathf.Atan2(diff.x, diff.y) * -180/Mathf.PI);		
+		arrow.eulerAngles = new Vector3(0,0,aimangle);
+		
+	}
+	
 }
