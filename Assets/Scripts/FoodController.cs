5f8c6f66a29f692d22dfdc35c2aad54c130f8721
Stuart King <king.s@husky.neu.edu>
2015-01-24 00:00:10 -0500

    Added pie physics

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodController : MonoBehaviour {
+
+	public Vector2 initialV;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddRelativeForce ((initialV * rigidbody2D.mass)/Time.fixedDeltaTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
