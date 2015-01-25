278edfc71dc8d0f9cc03f7fcfbfbf85ea1899e1a
pkenney <ricefrog@gmail.com>
2015-01-25 02:00:53 -0500

    Various tweaks to pie spawning, but where is the fun???

--- Assets/LDStarter/Scripts/LevelInstantiator.cs
+++ Assets/LDStarter/Scripts/LevelInstantiator.cs
@@ -12,28 +12,54 @@ public class LevelInstantiator  {
 // line for setting force on obj	rigidbody2D.AddRelativeForce ((initialV * rigidbody2D.mass) / Time.fixedDeltaTime);
 	public void InstantiateLevel(LevelData level)
 	{
+		Transform food;
+		FoodController fc;
+		/*
+		// LOW PIE:  This is to prevent the player from just pitching low-left every time
+		food = (Transform) GameObject.Instantiate (level.foodData[0]);
+		food.position = new Vector3(
+			(float)rand.NextDouble()* 3 + -20,
+			(float)rand.NextDouble()* 3 + 23,0);
+		fc = food.gameObject.GetComponent<FoodController>();		
+		fc.initialV = new Vector2(Random.Range(3,5),
+		                          Random.Range(-2,0));
+		
+		
+		// HIGH PIE:  This is to prevent the player from just standing still
+		food = (Transform) GameObject.Instantiate (level.foodData[0]);
+		food.position = new Vector3(
+			(float)rand.NextDouble()* 3 + -20,
+			(float)rand.NextDouble()* 3 + 23,0);
+		fc = food.gameObject.GetComponent<FoodController>();		
+		fc.initialV = new Vector2(Random.Range(10,12),
+		                          Random.Range(5,7));
+		
+		*/
+	
+		// COMMENT THIS SHIT - it's just to test the semi-fixed pies above w/out the randoms below.
+	
 		//variables for origin position of projectiles
-		float offsetX, offsetY, distribVar;
-		offsetX = -20;
-		offsetY = 10;
-		distribVar = 10;
-
+		float offsetX, offsetY, xdistribVar, ydistribVar;
+		offsetX = -25;
+		offsetY = 13;
+		xdistribVar = 10;
+		ydistribVar = 10;		
+		
 		//variables for input force of projectiles
 		float xvelMin, xvelMax, yvelMin, yvelMax;
-		xvelMin = 7;
+		xvelMin = 10;
 		xvelMax = 13;
-		yvelMin = 7;
-		yvelMax = 13;
+		yvelMin = -5;
+		yvelMax = 15;
 
-		//creates the list of food items with positions and angles
-		Transform food;
+		//creates the list of food items with positions and angles		
 		foreach (Transform t in level.foodData) {
 			food = (Transform) GameObject.Instantiate (t);
 			food.position = new Vector3(
-				(float)rand.NextDouble()* distribVar + offsetX,
-			    (float)rand.NextDouble()* distribVar + offsetY,0);
+				(float)rand.NextDouble()* xdistribVar + offsetX,
+			    (float)rand.NextDouble()* ydistribVar + offsetY,0);
 
-			FoodController fc = food.gameObject.GetComponent<FoodController>();
+			fc = food.gameObject.GetComponent<FoodController>();
 			
 			fc.initialV = new Vector2(Random.Range(xvelMin,xvelMax),
 			                          Random.Range(yvelMin,yvelMax));
