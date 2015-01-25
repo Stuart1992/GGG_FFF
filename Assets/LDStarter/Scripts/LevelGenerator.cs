879e93f756603a8f3a2a75dc5e817e7860422709
pkenney <ricefrog@gmail.com>
2015-01-25 12:14:29 -0500

    Added J-E-L-L-O

--- Assets/LDStarter/Scripts/LevelGenerator.cs
+++ Assets/LDStarter/Scripts/LevelGenerator.cs
@@ -18,7 +18,25 @@ public class LevelGenerator {
 		LevelData data = new LevelData(level);
 		data.StartPosition = new Vector2(1f,1f);
 		//generates list of incoming projectiles based off of level difficulty
-		int difficulty = (level + 3) / 2;
+		int difficulty;// = (level + 3) / 2;
+		if(level<=2)
+			difficulty = 2;
+		else if(level<=4)
+			difficulty = 3;
+		else if(level<=6)
+			difficulty=4;
+		else if((level>10) && (level%10==0))
+		{
+			difficulty = 10 + rand.Next(0,10);
+		}
+		else if((level>10) && (level%6==0))
+		{
+			difficulty = 8;
+		}		
+		else {
+			difficulty = Mathf.Min (6, 1 + Mathf.FloorToInt(level/2)) + rand.Next(-1,2);
+		}
+		
 		for (int i = 0; i < difficulty; i++) 
 		{
 			data.foodData.Add(foodItemPrefabs[Random.Range(0,foodItemPrefabs.Count)]);
---
+++ Assets/Scripts/JelloBounceWiggle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JelloBounceWiggle : MonoBehaviour {
+
+	private Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();		
+	}
+	
+	public void OnCollisionEnter2D(Collision2D col)
+	{
+		JumpInputController jic = col.gameObject.GetComponent<JumpInputController>();
+		if(col.gameObject.tag == "Ground")
+		{
+			anim.SetTrigger("Jiggle");
+		}
+	}
+}
